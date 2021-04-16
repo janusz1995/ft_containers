1bd27ce726889929a3301cb7f251b169b233979e
Ivan Skvortsov <vanskvrcv@gmail.com>
2021-04-16 20:01:10 +0300

    add more prints

--- utils/utilsVector.hpp
+++ utils/utilsVector.hpp
@@ -18,22 +18,22 @@ void printTwoVectors(ft::vector<T> &my, ft::vector<T> &my2) {
     std::cout << " |\n";
 }
 
-//template<typename T>
-//void printListsReverse(std::list<T> &list, ft::list<T> &myList) {
-//
-//    std::cout << BLUE << "Original List " << NC << " --->>> ";
-//    for (typename std::list<T>::reverse_iterator it = list.rbegin();  it != list.rend() ; it++) {
-//        std::cout << " | " << GREEN << *it << NC;
-//    }
-//    std::cout << " |\n";
-//
-//    std::cout << YELLOW << "My List " << NC << " --------->>> ";
-//    for (typename ft::list<T>::reverse_iterator it = myList.rbegin();  it != myList.rend() ; it++) {
-//        std::cout << " | " << GREEN << *it << NC;
-//    }
-//    std::cout << " |\n";
-//}
-//
+template<typename T>
+void printVectorsReverse(std::vector<T> &list, ft::vector<T> &myList) {
+
+    std::cout << BLUE << "Original Vector " << NC << " --->>> ";
+    for (typename std::vector<T>::reverse_iterator it = list.rbegin();  it != list.rend() ; it++) {
+        std::cout << " | " << GREEN << *it << NC;
+    }
+    std::cout << " |\n";
+
+    std::cout << YELLOW << "My Vector " << NC << " --------->>> ";
+    for (typename ft::vector<T>::reverse_iterator it = myList.rbegin();  it != myList.rend() ; it++) {
+        std::cout << " | " << GREEN << *it << NC;
+    }
+    std::cout << " |\n";
+}
+
 //template<typename T>
 //void printListsConstReverse(std::list<T> &list, ft::list<T> &myList) {
 //
@@ -89,41 +89,45 @@ void printVectors(std::vector<T> &original, ft::vector<T> &my) {
 //void printSize(std::string str, size_t size, std::string color) {
 //    std::cout << color << str << PURPLE << size << "\n" << NC;
 //}
-//
-//template<typename T>
-//void printTwoLists(std::list<T> &list, std::list<T> &list2, ft::list<T> &myList, ft::list<T> &myList2) {
-//
-//    std::cout << BLUE << "Original List 1 " << NC << " --->>> ";
-//    for (typename std::list<T>::iterator it = list.begin();  it != list.end() ; it++) {
-//        std::cout << " | " << GREEN << *it << NC;
-//    }
-//    std::cout << " |\n";
-//
-//    std::cout << BLUE << "Original List 2 " << NC << " --->>> ";
-//    for (typename std::list<T>::iterator it = list2.begin();  it != list2.end() ; it++) {
-//        std::cout << " | " << GREEN << *it << NC;
-//    }
-//    std::cout << " |\n";
-//
-//    printSize("Original List 1 Size: ", list.size(), ORIGINAL);
-//    printSize("Original List 2 Size: ", list2.size(), ORIGINAL);
-//
-//
-//    std::cout << YELLOW << "My List 1 " << NC << " --------->>> ";
-//    for (typename ft::list<T>::iterator it = myList.begin();  it != myList.end() ; it++) {
-//        std::cout << " | " << GREEN << *it << NC;
-//    }
-//    std::cout << " |\n";
-//
-//    std::cout << YELLOW << "My List 2 " << NC << " --------->>> ";
-//    for (typename ft::list<T>::iterator it = myList2.begin();  it != myList2.end() ; it++) {
-//        std::cout << " | " << GREEN << *it << NC;
-//    }
-//    std::cout << " |\n";
-//
-//    printSize("My List 1 Size:       ", myList.size(), MYCOLOR);
-//    printSize("My List 2 Size:       ", myList2.size(), MYCOLOR);
-//}
+
+template<typename T>
+void printTwoVectors(std::vector<T> &list, std::vector<T> &list2, ft::vector<T> &myList, ft::vector<T> &myList2) {
+
+    std::cout << BLUE << "Original Vector 1 " << NC << " --->>> ";
+    for (typename std::vector<T>::iterator it = list.begin();  it != list.end() ; it++) {
+        std::cout << " | " << GREEN << *it << NC;
+    }
+    std::cout << " |\n";
+
+    std::cout << BLUE << "Original Vector 2 " << NC << " --->>> ";
+    for (typename std::vector<T>::iterator it = list2.begin();  it != list2.end() ; it++) {
+        std::cout << " | " << GREEN << *it << NC;
+    }
+    std::cout << " |\n";
+
+    printSize("Original Vector 1 Size: ", list.size(), ORIGINAL);
+    printSize("Original Vector 2 Size: ", list2.size(), ORIGINAL);
+    printSize("Original Vector 1 Capacity: ", list.capacity(), ORIGINAL);
+    printSize("Original Vector 2 Capacity: ", list2.capacity(), ORIGINAL);
+
+
+    std::cout << YELLOW << "My Vector 1 " << NC << " --------->>> ";
+    for (typename ft::vector<T>::iterator it = myList.begin();  it != myList.end() ; it++) {
+        std::cout << " | " << GREEN << *it << NC;
+    }
+    std::cout << " |\n";
+
+    std::cout << YELLOW << "My Vector 2 " << NC << " --------->>> ";
+    for (typename ft::vector<T>::iterator it = myList2.begin();  it != myList2.end() ; it++) {
+        std::cout << " | " << GREEN << *it << NC;
+    }
+    std::cout << " |\n";
+
+    printSize("My Vector 1 Size: ", myList.size(), MYCOLOR);
+    printSize("My Vector 2 Size: ", myList2.size(), MYCOLOR);
+    printSize("My Vector 1 Capacity: ", myList.capacity(), MYCOLOR);
+    printSize("My Vector 2 Capacity: ", myList2.capacity(), MYCOLOR);
+}
 
 template<typename T>
 void capacityVectors(std::vector<T> &original, ft::vector<T> &copy) {

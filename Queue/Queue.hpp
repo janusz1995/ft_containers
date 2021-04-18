326c8c6c2c1bc494387f163f57f242bc54d41dec
Ivan Skvortsov <vanskvrcv@gmail.com>
2021-04-18 23:23:54 +0300

    queue done :)

--- Queue/Queue.hpp
+++ Queue/Queue.hpp
@@ -1,8 +1,102 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
+#include "../List/list.hpp"
 
-template < class T, class Alloc = std::allocator<T> > class queue {
+namespace ft {
 
-};
+    template<class T, class Container = ft::list<T> >
+    class queue {
+    public:
+        typedef T value_type;
+        typedef Container container_type;
+        typedef size_t size_type;
+
+        explicit queue(const container_type &ctnr = container_type()) : container(ctnr) {}
+
+        bool empty() const {
+            return (this->container.empty());
+        }
+
+        size_type size() const {
+            return (container.size());
+        }
+
+        value_type& front() {
+            return (container.front());
+        }
+
+        const value_type& front() const {
+            return (container.front());
+        }
+
+        value_type& back() {
+            return (container.back());
+        }
+
+        const value_type& back() const {
+            return (container.back());
+        }
+
+        void push(const value_type& val) {
+            this->container.push_back(val);
+        }
+
+        void pop() {
+            this->container.pop_front();
+        }
+
+        template <class A, class C>
+        friend bool operator==(const ft::queue<A, C>& lhs, const ft::queue<A, C>& rhs);
+
+        template <class A, class C>
+        friend bool operator!=(const ft::queue<A, C>& lhs, const ft::queue<A, C>& rhs);
+
+        template <class A, class C>
+        friend bool operator<(const ft::queue<A, C>& lhs, const ft::queue<A, C>& rhs);
+
+        template <class A, class C>
+        friend bool operator>(const queue<A, C>& lhs, const queue<A, C>& rhs);
+
+        template <class A, class C>
+        friend bool operator<=(const queue<A, C>& lhs, const queue<A, C>& rhs);
+
+        template <class A, class C>
+        friend bool operator>=(const queue<A, C>& lhs, const queue<A, C>& rhs);
+
+    private:
+        container_type container;
+
+    };
+
+    template <class T, class Container>
+    bool operator==(const queue<T,Container>& lhs, const queue<T,Container>& rhs) {
+        return (lhs.container == rhs.container);
+    }
+
+    template <class T, class Container>
+    bool operator!=(const queue<T,Container>& lhs, const queue<T,Container>& rhs) {
+        return (lhs.container != rhs.container);
+    }
+
+    template <class T, class Container>
+    bool operator<(const queue<T,Container>& lhs, const queue<T,Container>& rhs) {
+        return (lhs.container < rhs.container);
+    }
+
+    template <class T, class Container>
+    bool operator<=(const queue<T,Container>& lhs, const queue<T,Container>& rhs) {
+        return (lhs.container <= rhs.container);
+    }
+
+    template <class T, class Container>
+    bool operator>(const queue<T,Container>& lhs, const queue<T,Container>& rhs) {
+        return (lhs.container > rhs.container);
+    }
+
+    template <class T, class Container>
+    bool operator>=(const queue<T,Container>& lhs, const queue<T,Container>& rhs) {
+        return (lhs.container >= rhs.container);
+    }
+}
 
 #endif
--- Queue/mainQueue.cpp
+++ Queue/mainQueue.cpp
@@ -1,15 +1,110 @@
 #include "Queue.hpp"
 #include "../utils/utils.hpp"
+#include "../utils/utilsQueue.hpp"
 #include <iostream>
 #include <queue>
+#include "Queue.hpp"
+
 
 int main() {
 
 	std::queue<int> q;
+    ft::queue<int> myq;
+    ft::queue<int> myq1;
 
 
 
+    printMethod("EMPTY");
 
+    emptyQueue(q, myq);
+    sizeQueue(q, myq);
+
+
+
+    printMethod("PUSH AND BACK");
+
+    std::cout << BLUE << "Original Queue " << NC << " --->>> ";
+    for (int i = 0;  i < 10; i++) {
+        q.push(i * 10);
+        std::cout << " | " << GREEN << q.back() << NC;
+    }
+    std::cout << " |\n";
+
+    std::cout << YELLOW << "My Queue " << NC << " --------->>> ";
+    for (int i = 0;  i < 10; i++) {
+        myq.push(i * 10);
+        std::cout << " | " << GREEN << myq.back() << NC;
+    }
+    std::cout << " |\n";
+
+    emptyQueue(q, myq);
+    sizeQueue(q, myq);
+
+
+
+    printMethod("POP AND FRONT");
+
+    std::cout << BLUE << "Original Queue " << NC << " --->>> ";
+    for (int i = 0;  i < 10; i++) {
+        std::cout << " | " << GREEN << q.front() << NC;
+        q.pop();
+    }
+    std::cout << " |\n";
+
+    std::cout << YELLOW << "My Queue " << NC << " --------->>> ";
+    for (int i = 0;  i < 10 ; i++) {
+        std::cout << " | " << GREEN << myq.front() << NC;
+        myq.pop();
+    }
+    std::cout << " |\n";
+
+    emptyQueue(q, myq);
+    sizeQueue(q, myq);
+
+
+
+    printMethod("SIZE");
+
+    emptyQueue(q, myq);
+    sizeQueue(q, myq);
+
+
+
+    printMethod("RELATIONAL OPERATORS");
+
+    for (int i = 0; i < 3; ++i) {
+        myq.push(i + 1);
+        myq1.push(i + 1);
+    }
+
+    std::cout << RED << "*****" << CYAN << " Operator == " << RED << "*****\n";
+    if (myq == myq1)
+        std::cout << PURPLE << "Queue EQUALS\n";
+
+    std::cout << RED << "*****" << CYAN << " Operator <= " << RED << "*****\n";
+    if (myq <= myq1)
+        std::cout << PURPLE << "Queue 1 more or EQUAL Queue 2\n";
+
+    std::cout << RED << "*****" << CYAN << " Operator >= " << RED << "*****\n";
+    if (myq >= myq1)
+        std::cout << PURPLE << "Queue 2 more or EQUAL Queue 1\n";
+
+    myq1.pop();
+
+    std::cout << RED << "*****" << CYAN << " Operator != " << RED << "*****\n";
+    if (myq1 != myq)
+        std::cout << PURPLE << "Queue NOT EQUALS\n";
+    std::cout << RED << "*****" << CYAN << " Operator > " << RED << "*****\n";
+    if (myq1 > myq)
+        std::cout << PURPLE << "Queue 2 more than Queue 1\n";
+
+    myq.pop();
+    myq.pop();
+
+    std::cout << RED << "*****" << CYAN << " Operator < " << RED << "*****\n";
+    if (myq1 < myq) {
+        std::cout << PURPLE << "Queue 1 more than Queue 2\n";
+    }
 
 
 

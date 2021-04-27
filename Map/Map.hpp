9e4a7289b1452eb1a374a6322e7cbcb4e07d34f9
Ivan Skvortsov <vanskvrcv@gmail.com>
2021-04-27 22:21:59 +0300

    add normal insert :)

--- Map/Map.hpp
+++ Map/Map.hpp
@@ -20,291 +20,328 @@ namespace ft {
 			value_type data;
 
 		public:
-			mapNode(value_type &vt = value_type()):isBlack(false), left(NULL), right(NULL), parent(NULL), data(vt) {}
+			mapNode(const value_type &vt = value_type()):isBlack(false), left(NULL), right(NULL), parent(NULL), data(vt) {}
 
 
 	};
 
 	template <class T> class iteratorMap {
-	protected:
-		typedef T value_type;
-		typedef mapNode<T> node;
-		node *pointer;
-	public:
-//		template<typename myT, typename Alloc>
-//		friend class list;
+        protected:
+            typedef T value_type;
+            typedef mapNode<T> node;
+            node *pointer;
+            node *leaf;
+        public:
+    //		template<typename myT, typename Alloc>
+    //		friend class list;
 
-		iteratorMap(node *pointer = NULL): pointer(pointer) {}
+            iteratorMap(node *pointer = NULL, node *leaf = NULL): pointer(pointer), leaf(leaf) {}
 
-		bool operator==(iteratorMap const &itL) {
-			return (this->pointer == itL.pointer);
-		}
+            bool operator==(iteratorMap const &itL) {
+                return (this->pointer == itL.pointer); // TODO - add leaf ==
+            }
 
-		bool operator!=(iteratorMap const &itL) {
-			return (!(*this == itL));
-		}
+            bool operator!=(iteratorMap const &itL) {
+                return (!(*this == itL));
+            }
 
-		value_type& operator*() {
-			return (this->pointer->data);
-		}
+            value_type& operator*() {
+                return (this->pointer->data);
+            }
 
-		value_type* operator->() {
-			return (&(this->pointer->data));
-		}
+            value_type* operator->() {
+                return (&(this->pointer->data));
+            }
 
-		iteratorMap& operator++() { // ++it
-			this->pointer = this->pointer->next;
-			return (*this);
-		}
+            iteratorMap& operator++() { // ++it
+                this->pointer = tree_successor(this->pointer);
+                return (*this);
+            }
 
-		iteratorMap operator++(int) { // it++
-			iteratorMap it(*this);
-			this->pointer = this->pointer->next;
-			return (it);
-		}
-		iteratorMap& operator--() { // --it
-			this->pointer = this->pointer->prev;
-			return (*this);
-		}
+            iteratorMap operator++(int) { // it++
+                iteratorMap it(*this);
+                this->pointer = this->pointer->next;
+                return (it);
+            }
+            iteratorMap& operator--() { // --it
+                this->pointer = this->pointer->prev;
+                return (*this);
+            }
 
-		iteratorMap operator--(int) { // it--
-			iteratorMap it(*this);
-			this->pointer = this->pointer->prev;
-			return (it);
-		}
-		iteratorMap& operator=(const iteratorMap &it) {
-			this->pointer = it.pointer;
-			return (*this);
-		}
+            iteratorMap operator--(int) { // it--
+                iteratorMap it(*this);
+                this->pointer = this->pointer->prev;
+                return (it);
+            }
+            iteratorMap& operator=(const iteratorMap &it) {
+                this->pointer = it.pointer;
+                return (*this);
+            }
+
+	    private:
+            node* tree_successor(node *n) {
+                if (n->right != this->leaf) {
+                    return (tree_minimum(n->right));
+                }
+                node *y = n->parent;
+                while (y != this->leaf && n == y->right) {
+                    n = y;
+                    y = y->parent;
+                }
+                return (y);
+            }
+
+            node *tree_minimum(node *x) {
+                while (x->left != this->leaf) {
+                    x = x->left;
+                }
+                return (x);
+            }
 	};
 
-template < class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<std::pair<const Key,T> > > class map {
-	public:
-		typedef Key key_type;
-		typedef T mapped_type;
-		typedef Alloc allocator_type;
-		typedef size_t size_type;
-		typedef Compare key_compare;
-		typedef std::pair<const key_type, mapped_type> value_type;
-		typedef mapNode<value_type> node;
-		typedef iteratorMap<value_type> iterator;
-		typedef typename allocator_type::reference reference;
-		typedef typename allocator_type::const_reference const_reference;
-		typedef typename allocator_type::pointer pointer;
-		typedef typename allocator_type::const_pointer const_pointer;
+    template < class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<std::pair<const Key,T> > > class map {
+        public:
+            typedef Key key_type;
+            typedef T mapped_type;
+            typedef Alloc allocator_type;
+            typedef size_t size_type;
+            typedef Compare key_compare;
+            typedef std::pair<const key_type, mapped_type> value_type;
+            typedef mapNode<value_type> node;
+            typedef iteratorMap<value_type> iterator;
+            typedef typename allocator_type::reference reference;
+            typedef typename allocator_type::const_reference const_reference;
+            typedef typename allocator_type::pointer pointer;
+            typedef typename allocator_type::const_pointer const_pointer;
 
-	explicit map(const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) {
-		map_size = 0;
-		initNodes();
-	}
-
-//	template <class InputIterator>
-//	map(InputIterator first, InputIterator last, const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) {
-//
-//	}
-
-	map(const map& x) {
-
-	}
-
-	map& operator=(const map& x) {
-
-	}
-
-	~map() {
-		this->clear();
-	}
-
-	iterator begin() {}
-//	const_iterator begin() const {}
-
-	iterator end() {}
-//	const_iterator end() const {}
-
-//	reverse_iterator rbegin() {}
-//	const_reverse_iterator rbegin() const {}
-
-//	reverse_iterator rend() {}
-//	const_reverse_iterator rend() const {}
-
-	bool empty() const {
-		return (this->map_size == 0);
-	}
-
-	size_type size() const {
-		return (this->map_size);
-	}
-
-	size_type max_size() const {
-		return (std::numeric_limits<size_type>::max() / sizeof(value_type));
-	}
-
-	std::pair<iterator,bool> insert(const value_type& val) {
-
-		// TODO - if found val - return false
-
-		node *tmp = new node(val);
-		fillNode(tmp);
-		tmp->left = leaf;
-		tmp->right = leaf;
-
-		if (root->left == NULL) {
-			root->left = tmp;
-			return ;
-		}
-
-		node *current = this->root;
-		while (current != this->leaf) {
-            if (key_comp(current->data, val)) {
-                current = current->left;
-            } else if (key_comp(val, current->data)) {
-                current = current->right;
-            } else {
-                break;
-            }
-		}
-
-        if (key_comp(current->parent->data, val)) {
-            current->parent->left = tmp;
-        } else {
-            current->parent->right = tmp;
+        explicit map(const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) {
+            map_size = 0;
+            initNodes();
         }
 
-        tmp->parent = current->parent;
-		this->map_size++;
+    //	template <class InputIterator>
+    //	map(InputIterator first, InputIterator last, const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) {
+    //
+    //	}
 
-		return std::pair<iterator(tmp.pointer), true>;
-	}
+        map(const map& x) {
 
-	iterator insert(iterator position, const value_type& val) {
+        }
 
-	}
+        map& operator=(const map& x) {
 
-	template <class InputIterator>
-	void insert(InputIterator first, InputIterator last) {
+        }
 
-	}
+        ~map() {
+            this->clear();
+        }
 
+        iterator begin() {}
+    //	const_iterator begin() const {}
 
-	void erase(iterator position) {
+        iterator end() {}
+    //	const_iterator end() const {}
 
-	}
+    //	reverse_iterator rbegin() {}
+    //	const_reverse_iterator rbegin() const {}
 
-	size_type erase(const key_type& k) {
+    //	reverse_iterator rend() {}
+    //	const_reverse_iterator rend() const {}
 
-	}
+        bool empty() const {
+            return (this->map_size == 0);
+        }
 
-	void erase(iterator first, iterator last) {
+        size_type size() const {
+            return (this->map_size);
+        }
 
-	}
+        size_type max_size() const {
+            return (std::numeric_limits<size_type>::max() / sizeof(value_type));
+        }
 
-	void swap(map& x) {
+        std::pair<iterator,bool> insert(const value_type& val) {
 
-	}
+            if (this->root->left == NULL) {
+                node *tmp = new node(val);
+                fillNode(tmp);
+                tmp->left = this->leaf;
+                tmp->right = this->leaf;
+                this->root->left = tmp;
+                this->map_size++;
+                return (std::pair<iterator,bool>(iterator(tmp), true));
+            }
 
-	void clear() {
+            node *current = this->root;
+            while (current->right != this->leaf) {
+                if (current->data.first == val.first) {
+                    break;
+                }
 
-	}
+                if (this->comp(current->data.first, val.first) && current->left != this->leaf) {
+                    current = current->left;
+                } else if (current->right != this->leaf) {
+                    current = current->right;
+                }
+            }
 
-	key_compare key_comp() const {
+            if (current->data.first == val.first) {
+                return (std::pair<iterator,bool>(iterator(current), false));
+            }
 
-	}
+            node *tmp = new node(val);
+            fillNode(tmp);
+            tmp->left = this->leaf;
+            tmp->right = this->leaf;
 
-//	value_compare value_comp() const {}
+            if (this->comp(current->data.first, val.first)) {
+                current->left = tmp;
+            } else {
+                current->right = tmp;
+            }
 
-	iterator find(const key_type& k) {
+            tmp->parent = current;
+            this->map_size++;
 
-		if (this->map_size == 0) {
-			return NULL; 	// TODO: Return end()
-		}
+            return (std::pair<iterator,bool>(iterator(tmp), true));
+        }
 
-
-
-//		while (cur != leaf) {
+//        iterator insert(iterator position, const value_type& val) {
 //
-//		}
+//        }
+
+//        template <class InputIterator>
+//        void insert(InputIterator first, InputIterator last) {
+//
+//        }
 
 
-		return (0); // TODO: Return iterator
-	}
+        void erase(iterator position) {
 
-//	const_iterator find(const key_type& k) const {}
+        }
 
-	size_type count(const key_type& k) const {
+        size_type erase(const key_type& k) {
 
-	}
+        }
 
-//	iterator lower_bound(const key_type& k) {}
-//	const_iterator lower_bound(const key_type& k) const {}
+        void erase(iterator first, iterator last) {
 
-//	iterator upper_bound(const key_type& k) {}
-//	const_iterator upper_bound(const key_type& k) const {}
+        }
 
+        void swap(map& x) {
 
-//	pair<const_iterator,const_iterator> equal_range(const key_type& k) const {}
-//	pair<iterator,iterator> equal_range(const key_type& k) {}
+        }
 
+        void clear() {
 
+        }
 
-	private:
-		size_type map_size;
-		node *root;
-		node *leaf;
-		node *max;
-		node *min;
+        key_compare key_comp() const {
+            return (key_compare());
+        }
 
-		void initNodes() {
-			root = new node();
-			fill_node(root);
-			leaf = new node();
-			fill_node(leaf);
-			max = new node();
-			fill_node(max);
-			min = new node();
-			fill_node(min);
-//			root->parent = NULL;
-//			root->left = NULL;
-//			root->right = NULL;
+    //	value_compare value_comp() const {}
 
-		}
-
-		void fill_node(node *n) {
-			n->parent = NULL;
-			n->left = NULL;
-			n->right = NULL;
-		}
-
-		void findMin() {
-
-		    if (this->map_size == 0) {
-		        return;
-		    }
-
-		    node *current = this->root->left;
-
-		    while (current->left != this->leaf) {
-		        current = current->left;
-		    }
-
-		    this->min = current;
-		}
-
-        void findMax() {
+        iterator find(const key_type& k) {
 
             if (this->map_size == 0) {
-                return;
+                return NULL; 	// TODO: Return end() - max node
             }
 
-            node *current = this->root->left;
+            node *cur = this->root->left;
 
-            while (current->right != this->leaf) {
-                current = current->right;
+            while (cur != this->leaf && cur->data.first != k) {
+                if (key_comp()(cur->data.first, k)) {
+                    cur = cur->left;
+                } else {
+                    cur = cur->right;
+                }
+            }
+            if (cur == this->leaf) {
+                return (0); // TODO - end
             }
 
-            this->max = current;
+            return (iterator(cur)); // TODO: Return iterator
         }
 
-};
+    //	const_iterator find(const key_type& k) const {}
+
+        size_type count(const key_type& k) const {
+            return 0;
+        }
+
+    //	iterator lower_bound(const key_type& k) {}
+    //	const_iterator lower_bound(const key_type& k) const {}
+
+    //	iterator upper_bound(const key_type& k) {}
+    //	const_iterator upper_bound(const key_type& k) const {}
+
+
+    //	pair<const_iterator,const_iterator> equal_range(const key_type& k) const {}
+    //	pair<iterator,iterator> equal_range(const key_type& k) {}
+
+
+
+        private:
+            size_type map_size;
+            node *root;
+            node *leaf;
+            node *max;
+            node *min;
+            key_compare comp;
+
+            void initNodes() {
+                root = new node();
+                fillNode(root);
+                leaf = new node();
+                fillNode(leaf);
+                max = new node();
+                fillNode(max);
+                min = new node();
+                fillNode(min);
+    //			root->parent = NULL;
+    //			root->left = NULL;
+    //			root->right = NULL;
+
+            }
+
+            void fillNode(node *n) {
+                n->parent = NULL;
+                n->left = NULL;
+                n->right = NULL;
+            }
+
+            void findMin() {
+
+                if (this->map_size == 0) {
+                    return;
+                }
+
+                node *current = this->root->left;
+
+                while (current->left != this->leaf) {
+                    current = current->left;
+                }
+
+                this->min = current;
+            }
+
+            void findMax() {
+
+                if (this->map_size == 0) {
+                    return;
+                }
+
+                node *current = this->root->left;
+
+                while (current->right != this->leaf) {
+                    current = current->right;
+                }
+
+                this->max = current;
+            }
+
+    };
 
 	// map::key_type map::mapped_type map::key_compare map::allocator_type
 
--- Map/mapMain.cpp
+++ Map/mapMain.cpp
@@ -6,10 +6,14 @@ int main() {
 	std::map<int, std::string> original;
 	ft::map<int, std::string> copy;
 
+    original.insert(std::make_pair(1, "string"));
+    copy.insert(std::make_pair(1, "string"));
 
 
+    original.insert(std::make_pair(2, "sstring"));
+    copy.insert(std::make_pair(2, "sstring"));
 
-
+    std::cout << "Original size = " << original.size() << "; Copy size = " << copy.size() << std::endl;
 
 	return (0);
 }
